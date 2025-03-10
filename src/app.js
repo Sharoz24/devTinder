895a9c2aeac795e6134989c362336047ec9a27c4
Sharoz Khan <khansharoz2403@gmail.com>
2025-03-10 12:56:09 +0530

    writing-auth-middleware

--- src/app.js
+++ src/app.js
@@ -2,35 +2,25 @@ const express= require("express");
 
 const app= express();
 
-//app.use("/route", rH, [rH2, rH3], rH4, rH5);
+const {adminAuth,userAuth}= require("./middlewares/auth");
 
-app.get(
-    "/user",
-    [(req,res,next)=>{
-        console.log("Handling the route user!!");
-        next();
-    },
-    (req,res,next)=>{
-        console.log("Handling the route user 2!!");
-        // res.send("2nd Response");
-        next();
-    },
-    (req,res,next)=>{
-        console.log("handling the route user 3!!");
-        // res.send("3rd Response");
-        next();
-    },
-    (req,res,next)=>{
-        console.log("Handling the route user 4!!");
-        // res.send("4th Response");
-        next();
-    },
-    (req,res)=>{
-        console.log("Handling the route user 5!!");
-        res.send("5th Response")
+app.use("/admin", adminAuth);
 
-    }],
-);
+app.get("/user/data", userAuth, (req, res)=>{
+    res.send("User Data Sent")
+})
+
+app.post("/user/login", (req,res)=>{
+    res.send("User logged in succesfully!")
+})
+
+app.get("/admin/getAllData", (req,res)=>{
+    res.send("All Data sent")
+});
+
+app.get("/admin/deletedUser", (req,res)=>{
+    res.send("Deleted a user")
+})
 
 app.listen(7777, ()=>{
     console.log("Server is successfully listening on Port 7777")
