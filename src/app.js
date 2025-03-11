0331c4c8bc32f54065bc70e4a9988c1e075f32fb
Sharoz Khan <khansharoz2403@gmail.com>
2025-03-11 11:08:29 +0530

    playing-with-error-example

--- src/app.js
+++ src/app.js
@@ -2,25 +2,33 @@ const express= require("express");
 
 const app= express();
 
-const {adminAuth,userAuth}= require("./middlewares/auth");
 
-app.use("/admin", adminAuth);
 
-app.get("/user/data", userAuth, (req, res)=>{
-    res.send("User Data Sent")
-})
-
-app.post("/user/login", (req,res)=>{
-    res.send("User logged in succesfully!")
-})
-
-app.get("/admin/getAllData", (req,res)=>{
-    res.send("All Data sent")
+app.use("/", (err, req, res, next)=>{
+    if(err){
+         //Log on error
+        res.status(500).send("something went wrong")
+    }
 });
 
-app.get("/admin/deletedUser", (req,res)=>{
-    res.send("Deleted a user")
-})
+app.get("/getUserData", (req,res)=>{
+    // try{
+    //     //Logic of DB call and get user Data
+    throw new Error(tcyvuio);
+    //     res.send("User Data Sent")
+    // } catch(err){
+    //     res.status(500).send("Some Error contact support team");
+    // }
+});
+
+app.use("/", (err, req, res, next)=>{
+    if(err){
+         //Log on error
+        res.status(500).send("something went wrong1")
+    }
+});
+
+
 
 app.listen(7777, ()=>{
     console.log("Server is successfully listening on Port 7777")
