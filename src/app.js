6170539f67fa1a8cc2086fb7885bd347db6adc54
Sharoz Khan <khansharoz2403@gmail.com>
2025-03-13 07:07:02 +0530

    right-way to connect to DB

--- src/app.js
+++ src/app.js
@@ -1,35 +1,19 @@
 const express= require("express");
-
+const connectDB= require("./config/database")
 const app= express();
 
-app.use("/", (err, req, res, next)=>{
-    if(err){
-         //Log on error
-        res.status(500).send("something went wrong")
-    }
-});
 
-app.get("/getUserData", (req,res)=>{
-    // try{
-    //     //Logic of DB call and get user Data
-    throw new Error(tcyvuio);
-    res.send("User Data Sent")
-    // } catch(err){
-    //     res.status(500).send("Some Error contact support team");
-    // }
-});
-
-//You should
-app.use("/", (err, req, res, next)=>{
-    if(err){
-         //Log on error
-        res.status(500).send("something went wrong1")
-    }
-});
-
-
-
-app.listen(7777, ()=>{
-    console.log("Server is successfully listening on Port 7777")
+connectDB()
+.then(()=>{
+    console.log("Databse connection establised")
+    app.listen(7777, ()=>{
+        console.log("Server is successfully listening on Port 7777")
+    })
 })
+.catch((err)=>{
+    console.log("Database can't connected")
+});
+
+
+
 
